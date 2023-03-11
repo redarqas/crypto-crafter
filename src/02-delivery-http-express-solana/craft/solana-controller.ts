0e593014a7c6ee73067fb6be941bc9145e8df003
redarqas <jamal.chaqouri@gmail.com>
2023-03-11 11:21:10 +0100

    fix: Add broadcast api

--- src/02-delivery-http-express-solana/craft/solana-controller.ts
+++ src/02-delivery-http-express-solana/craft/solana-controller.ts
@@ -5,6 +5,7 @@ import { Router, Request, Response } from "express";
 import { SignInput } from "@org/core/sign";
 import { Boundary, CombineInputDetails, Intent } from "@org/core/solana";
 import { CombineInput } from "@org/core/combine";
+import { BroadcastInput } from "@org/core/broadcast";
 
 
 export class SolanaController implements Controller {
@@ -45,6 +46,17 @@ export class SolanaController implements Controller {
             }
             let input: CombineInput<CombineInputDetails> = req.body;
             await this.service.combine(network as Network, input)
+                .then(encoded => res.status(200).json(encoded))
+                .catch(next)
+        });
+
+        router.post('/solana/:network/broadcast', async (req: Request, res: Response, next) => {
+            let { network } = req.params;
+            if (!(network in Network)) {
+                return res.status(400).json({ error: 'Invalid network value' });
+            }
+            let input: BroadcastInput = req.body;
+            await this.service.broadcast(network as Network, input)
                 .then(encoded => res.status(200).json(encoded))
                 .catch(next)
         });
