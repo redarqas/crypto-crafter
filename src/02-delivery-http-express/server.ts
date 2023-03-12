69e51ae351a92c3fcc76d7fe44e9a1fea05ed048
redarqas <jamal.chaqouri@gmail.com>
2023-03-12 11:29:37 +0100

    feat(logging): Add logging http logging

--- src/02-delivery-http-express/server.ts
+++ src/02-delivery-http-express/server.ts
@@ -2,46 +2,36 @@ import { HttpApp } from "./http-app";
 import express from 'express';
 import bodyParser from 'body-parser';
 import listEndpoints from 'express-list-endpoints';
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
+const logger = expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        //winston.format.colorize(),
+        winston.format.json()
+    ),
+    msg: "HTTP {{req.method}} {{req.url}}",
+    colorize: false,
+})
 
 function notFoundMiddleware(req: express.Request, res: express.Response) {
     res.status(404).json({ message: 'Route not found' });
 }
-
-function requestLogger(req: express.Request, res: express.Response, next: express.NextFunction) {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-    console.log('Request body:', req.body);
-    next();
-}
-
-function responseLogger(req: express.Request, res: express.Response, next: express.NextFunction) {
-    const originalSend = res.send;
-    res.send = function (body) {
-        console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} ${res.statusCode}`);
-        console.log('Response body:', body);
-        originalSend.call(res, body);
-        return res;
-    };
-    next();
-}
-
 
 export const Server = {
     serve: function (app: HttpApp, port: number) {
         const serverBuilder = express();
         serverBuilder.use(express.json());
         serverBuilder.use(bodyParser.json());
-        serverBuilder.use(requestLogger);
+        serverBuilder.use(logger);
         serverBuilder.use(app.routes());
-        serverBuilder.use(responseLogger);
         serverBuilder.use(notFoundMiddleware);
         console.log(listEndpoints(serverBuilder));
         serverBuilder.listen(port, () => {
             console.log(`Server is listening on port ${port}`);
         });
 
+
     }
 }
-
-
-
